7fccfd7c5b8abab26e4e5edd3ad0f48c37bccffd
ezehlivinus <ezehlivinusezeh@gmail.com>
2022-08-10 11:02:17 +0100

    create index.js and home.ts added some variables

---
+++ home.ts
@@ -0,0 +1,7 @@
+let firstName: string = 'John';
+let age: number = 30;
+let lastname: string = 'Doe';
+console.warn('Last name variable is in camel case', lastname);
+if (typeof(age) !== 'number') {
+  console.error('Age is not a number');
+}
